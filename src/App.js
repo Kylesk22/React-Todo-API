320a775bbd16137577d12ace62362e47d3f0f3ee
Kylesk22 <105800286+Kylesk22@users.noreply.github.com>
2022-09-18 00:41:21 -0400

    ToDo List Final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import TodoList from './Components/TodoList/todoList';
+import './bootstrap.min (2).css'
+
 
 function App() {
+  const [input, setInput] = useState("");
+  const [list, setList] = useState([]);
+  const [closeButton, setCloseButton] = useState(null);
+
+  useEffect(() => {
+    setList([...list, input])
+  }, [input])
+
+  function removeItem(index) {
+    console.log(index);
+    list.splice(index,1);
+    setCloseButton(false);
+    
+
+  }
+  function alertText() {
+    let liLength = list.length - 1;
+    if (liLength !== 0) {
+      return `${liLength} items remaining`
+    }
+    else return `No tasks, add a task.`
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODOS</h1>
+      <TodoList setInput={setInput} input={input}/>
+      
+        <ul>
+          {list.map((val, index) => {
+            if (val !== "") {
+            return (
+                <li key={index} id = {index} onMouseOver={(e) => {setCloseButton(index)}} onMouseOut={()=>setCloseButton(false)}>
+                  {(closeButton === index)? <button className="btn btn-danger" style={{float: 'right'}} onClick= {()=>removeItem(index)}>X</button>: null}
+                  {val}
+                  </li>
+            )
+}})}
+            <li className="remaining">{alertText()} </li>
+            <li className="extra1"></li>
+            <li className="extra2"></li>
+
+        </ul>
+      
     </div>
   );
 }
---
+++ src/Components/TodoList/todoList.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./todoList.css";
+
+const TodoList = (props) => {
+    const [todo, setTodo] = useState("");
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (todo !== "") {
+        props.setInput(todo);
+        console.log(todo);
+        setTodo("");}
+    }
+
+
+    return(
+    <>
+    <form className="container" onSubmit={onSubmit}>
+        <input type="text" className="form-control" placeholder="What needs to be done?" aria-label="Todo" aria-describedby="button-addon2" value = {todo} onChange={(e) => {setTodo(e.target.value); console.log(todo)}}/>
+
+    </form>
+    </>
+    )
+}
+
+export default TodoList;
